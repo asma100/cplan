974e503202afdd92c2ddbab71d7d70ef08cc794e
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 16:22:41 +0300

    Update parse.c

--- parse.c
+++ parse.c
@@ -5,76 +5,9 @@ int tok_counter = 0;
 char **arr = NULL;
 int u, i, j;
 char *input_cp = strdup(input);
-  handle_inputerror(char *input, char *input_cp)
-{
-if (input_cp == NULL)
-{
-perror("Error allocating memory:");
-free(input);
-return;
+  /*handle_inputerror(input, input_cp);*/
+tok_counter = counter(char *input, const char *delim);
+arr = split(char *input_cp, const char *delim, tok_counter);
+executop(arr);
+free_after_exe(input, arr);
 }
-if (input[0] == '\0')
-{
-free(input_cp);
-return;
-
-}
-}
-  
-counter(char *input, const char *delim)
-{
-  tok = strtok(input, delim);
-while (tok != NULL)
-{
-tok_counter++;
-tok = strtok(NULL, delim);
-}
-tok_counter++;
-}
-
-
-
-arr = malloc(sizeof(char *) * tok_counter);
-if (arr == NULL)
-{
-perror("Error allocating memory for arr:");
-free(input);
-free(input_cp);
-return;
-}
-  split(char *input_cp, const char *delim )
-{
-  tok = strtok(input_cp, delim);
-for (u = 0; tok != NULL; u++)
-{
-arr[u] = malloc(sizeof(char) * (strlen(tok) + 2));
-if (arr[u] == NULL)
-{
-perror("Error allocating memory for arr[u]:");
-for (j = 0; j < u; j++)
-{
-free(arr[j]);
-}
-free(arr);
-free(input);
-free(input_cp);
-return;
-}
-strcpy(arr[u], tok);
-tok = strtok(NULL, delim);
-}
-}
-
-arr[u] = NULL;
-topcmd(arr);
- free_after_exe(char *input, char **arr);
-{
-for (i = 0; i < u; i++)
-{
-free(arr[i]);
-}
-free(arr);
-free(input_cp);
- }
-}
-

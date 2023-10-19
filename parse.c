4ad7579d4e70882833ec4e21b92e27d60fab0c3d
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 16:56:50 +0300

    Update parse.c

--- parse.c
+++ parse.c
@@ -4,11 +4,39 @@ void Parse(char *input, const char *delim)
 
 int tok_counter = 0;
 char **arr = NULL;
-
+int u, j;
+char *tok;
 char *input_cp = strdup(input);
   /*handle_inputerror(input, input_cp);*/
 tok_counter = counter(input, delim);
-arr = split(input_cp, delim, tok_counter);
+arr = malloc(sizeof(char *) * tok_counter);
+if (arr == NULL)
+{
+perror("Error allocating memory for arr:");
+free(input);
+free(input_cp);
+return;
+}
+  tok = strtok(input_cp, delim);
+for (u = 0; tok != NULL; u++)
+{
+arr[u] = malloc(sizeof(char) * (strlen(tok) + 2));
+if (arr[u] == NULL)
+{
+perror("Error allocating memory for arr[u]:");
+for (j = 0; j < u; j++)
+{
+free(arr[j]);
+}
+free(arr);
+free(input);
+free(input_cp);
+return(NULL);
+}
+strcpy(arr[u], tok);
+tok = strtok(NULL, delim);
+}
+arr[u] = NULL;
 execmd(arr);
-free_after_exe(input, arr,u);
+free_after_exe(input, arr, u);
 }

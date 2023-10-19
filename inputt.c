8ab76074329e8d83b2966acc5974c46ddce6089a
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 15:05:06 +0300

    Update inputt.c

--- inputt.c
+++ inputt.c
@@ -1,6 +1,10 @@
 #include "top.h"
 
-
+/**
+ * empty - function for the user input handling
+ * @input: size of the input
+ * Return: int
+*/
 int empty(char *input)
 {
 	if (input == NULL)
@@ -15,7 +19,13 @@ int empty(char *input)
 
 	return (1);
 }
-
+/**
+ * inputtop - function for the user input handling
+ * @s: size of the input
+ * @input: value
+ * @status:0,1
+ * Return: null
+*/
 void inputtop(char *input, size_t s, int status) {
 	ssize_t checkline;
 	char exitstatus[] = "/bin/ls: cannot access '/test_hbtn': No such file or directory\n";

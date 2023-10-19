bf61ff302d8c7c3c9a1408d69b3da5e2829389d7
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 16:53:12 +0300

    Update parsing_funactions.c

--- parsing_funactions.c
+++ parsing_funactions.c
@@ -31,42 +31,8 @@ return (tok_counter);
 }
 
 
- char **split(char *input_cp, const char *delim,int tok_counter)
-{ 
-int u, j;
-char *tok;
-arr = malloc(sizeof(char *) * tok_counter);
-if (arr == NULL)
-{
-perror("Error allocating memory for arr:");
-free(input);
-free(input_cp);
-return;
-}
-  tok = strtok(input_cp, delim);
-for (u = 0; tok != NULL; u++)
-{
-arr[u] = malloc(sizeof(char) * (strlen(tok) + 2));
-if (arr[u] == NULL)
-{
-perror("Error allocating memory for arr[u]:");
-for (j = 0; j < u; j++)
-{
-free(arr[j]);
-}
-free(arr);
-free(input);
-free(input_cp);
-return(NULL);
-}
-strcpy(arr[u], tok);
-tok = strtok(NULL, delim);
-}
-arr[u] = NULL;
-return (arr);
-}
 
-voide  free_after_exe(char *input, char **arr)
+voide  free_after_exe(char *input, char **arr,int u)
 {
 int i;
 for (i = 0; i < u; i++)

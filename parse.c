91d68ad388643ab1f3ce1732cc7ba6343e0d9639
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 16:43:03 +0300

    Update parse.c

--- parse.c
+++ parse.c
@@ -7,8 +7,8 @@ char **arr = NULL;
 
 char *input_cp = strdup(input);
   /*handle_inputerror(input, input_cp);*/
-tok_counter = counter(char *input, const char *delim);
-arr = split(char *input_cp, const char *delim, tok_counter);
+tok_counter = counter(input, delim);
+arr = split(input_cp, delim, tok_counter);
 execmd(arr);
 free_after_exe(input, arr);
 }
